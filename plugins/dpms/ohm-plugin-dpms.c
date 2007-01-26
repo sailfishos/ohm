9457c7ef582a76c438efdd2af14a7de31b6f30ce
Richard Hughes <richard@hughsie.com>
2007-01-26 02:10:22 +0000

    fix the dpms plugin
    Fix the dpms plugin, and only compile and install it if we have the DPMS
    headers on the box. Using DPMS adds memory requriement by about 20k, wow.

--- plugins/dpms/ohm-plugin-dpms.c
+++ plugins/dpms/ohm-plugin-dpms.c
@@ -27,13 +27,11 @@
 
 #include <ohm-plugin.h>
 
-#ifdef HAVE_DPMS_EXTENSION
 #include <X11/Xutil.h>
 #include <X11/Xproto.h>			/* for CARD16 */
 #include <X11/extensions/dpms.h>
 #include <X11/Xlib.h>
 #include <X11/extensions/dpmsstr.h>
-#endif
 
 enum {
 	CONF_BACKLIGHT_STATE_CHANGED,
@@ -49,13 +47,13 @@ typedef enum {
 
 typedef struct {
 	gint state;
+	Display *dpy;
 } OhmPluginCacheData;
 
 OhmPluginCacheData data;
 
 /**
  * plugin_preload:
- * @plugin: This class instance
  *
  * Called before the plugin is coldplg.
  * Define any modules that the plugin depends on, but do not do coldplug here
@@ -64,10 +62,6 @@ OhmPluginCacheData data;
 static gboolean
 plugin_preload (OhmPlugin *plugin)
 {
-#ifndef HAVE_DPMS_EXTENSION
-	/* no point loading this plugin if we have no DPMS support */
-	return FALSE;
-#endif
 	/* add in the required, suggested and prevented plugins */
 	ohm_plugin_require (plugin, "backlight");
 	return TRUE;
@@ -78,28 +72,24 @@ plugin_preload (OhmPlugin *plugin)
  * ohm_dpms_get_mode:
  */
 static gboolean
-ohm_dpms_get_mode (OhmDpmsMode *mode, const char *display_name)
+ohm_dpms_get_mode (OhmDpmsMode *mode)
 {
-#ifdef HAVE_DPMS_EXTENSION
 	OhmDpmsMode result;
 	int event_number;
 	int error_number;
 	BOOL enabled = FALSE;
-	Display *dpy;
 	CARD16 state;
 
-	dpy = XOpenDisplay (display_name);
-
-	if (! DPMSQueryExtension (dpy, &event_number, &error_number)) {
+	if (! DPMSQueryExtension (data.dpy, &event_number, &error_number)) {
 		/* Server doesn't know -- assume the monitor is on. */
 		result = OHM_DPMS_MODE_ON;
 
-	} else if (! DPMSCapable (dpy)) {
+	} else if (! DPMSCapable (data.dpy)) {
 		/* Server says the monitor doesn't do power management -- so it's on. */
 		result = OHM_DPMS_MODE_ON;
 
 	} else {
-		DPMSInfo (dpy, &state, &enabled);
+		DPMSInfo (data.dpy, &state, &enabled);
 		if (! enabled) {
 			/* Server says DPMS is disabled -- so the monitor is on. */
 			result = OHM_DPMS_MODE_ON;
@@ -127,7 +117,6 @@ ohm_dpms_get_mode (OhmDpmsMode *mode, const char *display_name)
 	if (mode) {
 		*mode = result;
 	}
-#endif
 	return TRUE;
 }
 
@@ -135,20 +124,17 @@ ohm_dpms_get_mode (OhmDpmsMode *mode, const char *display_name)
  * ohm_dpms_set_mode:
  */
 static gboolean
-ohm_dpms_set_mode (OhmDpmsMode mode, const char *display_name)
+ohm_dpms_set_mode (OhmDpmsMode mode)
 {
-#ifdef HAVE_DPMS_EXTENSION
-	Display *dpy;
 	CARD16 state;
 	OhmDpmsMode current_mode;
 
-	dpy = XOpenDisplay (display_name);
-	if (dpy == NULL) {
+	if (data.dpy == NULL) {
 		g_debug ("cannot open display");
 		return FALSE;
 	}
 
-	if (! DPMSCapable (dpy)) {
+	if (! DPMSCapable (data.dpy)) {
 		g_debug ("display not DPMS capable");
 		return FALSE;
 	}
@@ -167,34 +153,31 @@ ohm_dpms_set_mode (OhmDpmsMode mode, const char *display_name)
 	}
 
 	/* make sure we are not trying to set the current screen mode, else we flicker */
-	ohm_dpms_get_mode (&current_mode, display_name);
+	ohm_dpms_get_mode (&current_mode);
 	if (current_mode != mode) {
 		g_debug ("Setting DPMS state");
-		if (! DPMSForceLevel (dpy, state)) {
+		if (! DPMSForceLevel (data.dpy, state)) {
 			g_warning ("Could not change DPMS mode");
 			return FALSE;
 		}
 	}
 
-	XSync (dpy, FALSE);
-#endif
+	XSync (data.dpy, FALSE);
 	return TRUE;
 }
 
 /**
  * check_system_dpms_state:
- * @plugin: This class instance
  *
  * Check the battery, and set the low and critical values if battery is low
  */
 static void
 check_system_dpms_state (OhmPlugin *plugin)
 {
-	/* fixme: don't assume :0 */
 	if (data.state == 0) {
-		ohm_dpms_set_mode (OHM_DPMS_MODE_OFF, ":0");
+		ohm_dpms_set_mode (OHM_DPMS_MODE_OFF);
 	} else if (data.state == 1) {
-		ohm_dpms_set_mode (OHM_DPMS_MODE_ON, ":0");
+		ohm_dpms_set_mode (OHM_DPMS_MODE_ON);
 	} else {
 		g_error ("unknown mode, can't set with DPMS");
 	}
@@ -202,7 +185,6 @@ check_system_dpms_state (OhmPlugin *plugin)
 
 /**
  * plugin_coldplug:
- * @plugin: This class instance
  *
  * Coldplug, i.e. read and set the initial state of the plugin.
  * We can assume all the required modules have been loaded, although it's
@@ -217,12 +199,26 @@ plugin_coldplug (OhmPlugin *plugin)
 	/* initial values */
 	data.state = 1;
 
+	/* open display, need to free using XCloseDisplay */
+	data.dpy = XOpenDisplay (":0"); /* fixme: don't assume :0 */
+
 	check_system_dpms_state (plugin);
 }
 
 /**
+ * plugin_unload:
+ *
+ * Unload drivers, free memory.
+ */
+static void
+plugin_unload (OhmPlugin *plugin)
+{
+	XCloseDisplay (data.dpy);
+	data.dpy = NULL;
+}
+
+/**
  * plugin_conf_notify:
- * @plugin: This class instance
  *
  * Notify the plugin that a key marked with ohm_plugin_conf_interested ()
  * has it's value changed.
@@ -242,7 +238,7 @@ static OhmPluginInfo plugin_info = {
 	"0.0.1",			/* version */
 	"richard@hughsie.com",		/* author */
 	plugin_preload,			/* preload */
-	NULL,				/* unload */
+	plugin_unload,			/* unload */
 	plugin_coldplug,		/* coldplug */
 	plugin_conf_notify,		/* conf_notify */
 };
