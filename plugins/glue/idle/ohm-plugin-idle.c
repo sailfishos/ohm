80ead8dfca7bc850e3ca914938772d2fa20a3965
Richard Hughes <richard@hughsie.com>
2007-07-10 00:36:16 +0100

    add initial idletime detection for olpc
    We still need to check for the presence of IDLETIME and warn if not present

--- plugins/glue/idle/ohm-plugin-idle.c
+++ plugins/glue/idle/ohm-plugin-idle.c
@@ -22,6 +22,10 @@
 #include <glib.h>
 
 #include <ohm-plugin.h>
+#include <libidletime.h>
+#include <gdk/gdk.h>
+
+static LibIdletime *idletime;
 
 /**
  * plugin_preload:
@@ -35,8 +39,28 @@ static gboolean
 plugin_preload (OhmPlugin *plugin)
 {
 	/* tell ohmd what keys we are going to provide */
-	ohm_plugin_conf_provide (plugin, "idle.system_idle");
+	ohm_plugin_conf_provide (plugin, "idle.is_momentary");
+	ohm_plugin_conf_provide (plugin, "idle.is_powersave");
+	ohm_plugin_conf_provide (plugin, "idle.is_powerdown");
 	return TRUE;
+}
+
+static void
+ohm_alarm_expired_cb (LibIdletime *idletime, guint alarm, gpointer data)
+{
+	OhmPlugin *plugin = (OhmPlugin *) data;
+	if (alarm == 0) {
+		ohm_plugin_conf_set_key (plugin, "idle.is_momentary", 0);
+		ohm_plugin_conf_set_key (plugin, "idle.is_powersave", 0);
+		ohm_plugin_conf_set_key (plugin, "idle.is_powerdown", 0);
+	} else if (alarm == 1) {
+		ohm_plugin_conf_set_key (plugin, "idle.is_momentary", 1);
+	} else if (alarm == 2) {
+		ohm_plugin_conf_set_key (plugin, "idle.is_powersave", 1);
+	} else if (alarm == 3) {
+		ohm_plugin_conf_set_key (plugin, "idle.is_powerdown", 1);
+	}
+	g_print ("[evt %i]\n", alarm);
 }
 
 /**
@@ -50,14 +74,49 @@ plugin_preload (OhmPlugin *plugin)
 static void
 plugin_coldplug (OhmPlugin *plugin)
 {
+	gboolean ret;
+	gint momentary;
+	gint powersave;
+	gint powerdown;
+
+	gdk_init (NULL, NULL);
+	idletime = idletime_new ();
+	if (idletime == NULL) {
+		g_error ("cannot create idletime");
+	}
+	g_signal_connect (idletime, "alarm-expired",
+			  G_CALLBACK (ohm_alarm_expired_cb), plugin);
+
+	ohm_plugin_conf_get_key (plugin, "idle.timer_momentary", &momentary);
+	ohm_plugin_conf_get_key (plugin, "idle.timer_powersave", &powersave);
+	ohm_plugin_conf_get_key (plugin, "idle.timer_powerdown", &powerdown);
+
+	ret = idletime_alarm_set (idletime, 1, momentary);
+	if (ret == FALSE) {
+		g_error ("cannot set alarm 1");
+	}
+	ret = idletime_alarm_set (idletime, 2, powersave);
+	if (ret == FALSE) {
+		g_error ("cannot set alarm 2");
+	}
+	ret = idletime_alarm_set (idletime, 3, powerdown);
+	if (ret == FALSE) {
+		g_error ("cannot set alarm 3");
+	}
+}
+
+static void
+plugin_unload (OhmPlugin *plugin)
+{
+	g_object_unref (idletime);
 }
 
 static OhmPluginInfo plugin_info = {
-	"OHM PowerStatus",		/* description */
+	"OHM IdleTime",		/* description */
 	"0.0.1",			/* version */
 	"richard@hughsie.com",		/* author */
 	plugin_preload,			/* preload */
-	NULL,				/* unload */
+	plugin_unload,			/* unload */
 	plugin_coldplug,		/* coldplug */
 	NULL,				/* conf_notify */
 };
