87c1a4c36f0a2f08b66101f335ea9cecfd77fafa
Rob Taylor <rob.taylor@codethink.co.uk>
2007-08-03 14:50:45 +0100

    make run-ohm.sh more capable
    Adds options --memcheck and --massif for running ohmd with the corresponding
    valgrid tools. Also adds a --help option.

--- ohmd/run-ohm.sh
+++ ohmd/run-ohm.sh
@@ -2,6 +2,22 @@
 
 OHM_TMPDIR=/tmp/run-ohmd-$USER
 
+if [ "$1" = "--help" ]; then
+	echo "Usage:"
+	echo "  $0 [run-options] [ohm-options]"
+	echo
+	echo "Run Options:"
+	echo "  --skip-plugin-install: Don't do a temporary install of the plugins."
+	echo "                         Only use this if $0 has already been run"
+	echo "                         without this option"
+	echo "  --debug:               Run with gdb"
+	echo "  --memcheck:            Run with valgrind memcheck tool"
+	echo "  --massif:              Run with valgrind massif heap-profiling tool"
+	echo
+	./ohmd --help
+	exit 0
+fi
+
 if [ "$1" = "--skip-plugin-install" ] ; then
 	shift
 else
@@ -11,12 +27,17 @@ fi
 
 if [ "$1" = "--debug" ] ; then
 	shift
-	commandline="sudo gdb --args ./ohmd --no-daemon --verbose $@"
-else
-	commandline="sudo ./ohmd --no-daemon --verbose $@"
+	prefix="gdb --args"
+elif [ "$1" = "--memcheck" ] ; then
+	shift
+	prefix="valgrind --show-reachable=yes --leak-check=full --tool=memcheck"
+elif [ "$1" = "--massif" ] ; then
+	shift
+	prefix="valgrind --tool=massif"
 fi
 
 export OHM_CONF_DIR=$OHM_TMPDIR/etc/ohm
 export OHM_PLUGIN_DIR=$OHM_TMPDIR/lib/ohm
 
-$commandline
+echo "Execing: $prefix ./ohmd --no-daemon --verbose $@"
+sudo $prefix ./ohmd --no-daemon --verbose $@
