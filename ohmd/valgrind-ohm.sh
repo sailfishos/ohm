fd6a9c0525726aa3a030727e779d8f03a454b72d
Rob Taylor <rob.taylor@codethink.co.uk>
2007-08-08 16:22:23 +0100

    remove unneeded scripts
    Removes massif-ohm.sh and valgrind-ohm.sh as no longer needed.

--- ohmd/valgrind-ohm.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#valgrind --num-callers=20 --show-reachable=yes --leak-check=yes --tool=memcheck ./ohmd --no-daemon --timed-exit $@
-valgrind --show-reachable=yes --tool=memcheck --leak-check=full ./ohmd --no-daemon --timed-exit $@
