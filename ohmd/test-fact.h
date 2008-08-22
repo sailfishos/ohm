7be64b6e83789367448b3eaa0ba7ef473c745064
Krisztian Litkey <kli@kliptop.localdomain>
2008-08-22 17:34:38 +0300

    Build the factstore tests.

---
+++ ohmd/test-fact.h
@@ -0,0 +1,17 @@
+
+#ifndef __TESTS_TEST_FACT_H__
+#define __TESTS_TEST_FACT_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+
+
+void _main (char** args, int args_length1);
+
+
+G_END_DECLS
+
+#endif
