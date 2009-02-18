1358f861ea861af4308ed729a7b8c51614deb025
Krisztian Litkey <kli@kliptop.localdomain>
2009-02-18 14:17:14 +0200

    Changed to use the rewritten trace library.

--- include/ohm/ohm-plugin-debug.h
+++ include/ohm/ohm-plugin-debug.h
@@ -1,27 +1,27 @@
 #ifndef __OHM_PLUGIN_DEBUG_H__
 #define __OHM_PLUGIN_DEBUG_H__
 
-#include <trace/trace.h>
+#include <simple-trace/simple-trace.h>
 
 #ifndef unlikely
 #  define unlikely(cond) __builtin_expect(cond, 0)
 #endif
 
 #define OHM_DEBUG_PLUGIN(plugin, ...)				\
-    TRACE_DECLARE_COMPONENT(plugin, #plugin, __VA_ARGS__)
+    TRACE_DECLARE_MODULE(plugin, #plugin, __VA_ARGS__)
 
 #define OHM_DEBUG_FLAG(n, d, br)		\
-    TRACE_FLAG_INIT((n), (d), (br))
+    TRACE_FLAG((n), (d), (br))
 
 #define OHM_DEBUG_INIT(plugin) ({					\
       int success;							\
-      success = !trace_init() && !trace_add_component(NULL, &plugin);	\
+      success = !trace_init() &&					\
+	!trace_add_module(TRACE_DEFAULT_CONTEXT, &plugin);		\
       success;								\
     })
 
 #define OHM_DEBUG(flag, format, args...) do {				\
-    __trace_write(NULL, __FILE__, __LINE__, __FUNCTION__,		\
-		  flag, NULL, format"\n", ## args);			\
+    trace_printf((flag), format , ## args);				\
   } while (0)
 
 #define OHM_DEBUG_ON(f)     trace_on(NULL, f)
